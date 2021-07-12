5d9892912028210b8958646f404598f3089e9482
junsam.ji <cowtree84@gmail.com>
2021-07-12 18:19:09 +0900

    국내현황 업데이트

--- src/pages/Sub.js
+++ src/pages/Sub.js
@@ -3,17 +3,33 @@ import './Sub.css';
 import Covid from '../components/Covid';
 
 class Sub extends React.Component {
+    state = { 
+      country : "KOR", 
+    };   
+
+    handleClick = () => {
+      setTimeout(this.showMessage, 3000);
+    };
+
+    showMessage = () => {
+      alert('flolowed ' + this.state.country);
+    };
+
     render() {
         return(
         <div>
           <header className="header">
             <h1>COVID-19</h1>
-            <select>
-                <option>국내</option>
-                <option>세계</option>
+            <select value={ this.state.country } onChange={e => this.setState({ country: e.target.value })}>
+                <option value="KOR">국내</option>
+                <option value="WORLD">해외</option>
             </select>
           </header>
-          <Covid />
+          <div>
+          {
+            this.state.country === "KOR" ? <Covid /> : <p>현재는 국내만 확인 가능합니다.</p>
+          }
+          </div>
         </div>
         );
     }
