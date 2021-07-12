48c514a677fbfca8ed20b80716dab43f4a9a12f0
junsam.ji <cowtree84@gmail.com>
2021-07-12 16:18:29 +0900

    chart add

--- src/pages/Sub.js
+++ src/pages/Sub.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Sub.css';
+import Covid from '../components/Covid';
 
 class Sub extends React.Component {
     render() {
         return(
+        <div>
           <header className="header">
             <h1>COVID-19</h1>
             <select>
                 <option>국내</option>
                 <option>세계</option>
             </select>
-        </header>
+          </header>
+          <Covid />
+        </div>
         );
     }
 }
