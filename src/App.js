847c6a945278ce4ef3887b16be298a669a3cb41d
junsam.ji <cowtree84@gmail.com>
2021-07-14 18:29:23 +0900

    header test

--- src/App.js
+++ src/App.js
@@ -1,54 +1,67 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import {Animated} from 'react-animated-css';
-import './App.css';
-/*import Header from './Header';*/
-import { Home, About, Sub } from './';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
 
-function App() {
-  const [text, setText] = useState("Portfolio");
+const App = () => (
+  <StickyHeader
+    // This is the sticky part of the header.
+    header={
+      <div className="Header_root">
+        <h1 className="Header_title">ReactStickyHeader</h1>
 
-  return (
-    <div>
-      <Router basename="/portfolio-react">
-        {/*<Header />*/}
-      <div className="sticky">
-        <Animated animationIn="bounceInLeft" animationOUt="fadeOut" isVisible={true}>
-          <div>
-              <span className="App-header">
-                  <span className="App-content">{text}</span>                    
-                  {/*<span className='App-menu'><img src={menu} alt="menu" /></span>*/}
-              </span>
-          </div>
-        </Animated>
-
-        <Animated animationIn="bounceInRight" animationOUt="fadeOut" isVisible={true}>
-          <div className="sticky">
-            <span className='App-header'>
-                <span className='App-content'>
-                  <Link to="/">Home</Link>
-                </span>
-                
-                <span className='App-content'>
-                  <Link to="/about">Map</Link>
-                </span>
-              
-                <span className='App-content'>
-                  <Link to="/sub">Corona</Link>
-                </span>
-            </span>   
-          </div>
-        </Animated>
+        <ul className="Header_links">
+          <li className="Header_link">When</li>
+          <li className="Header_link">Why</li>
+          <li className="Header_link">About</li>
+        </ul>
       </div>
-
-      <div>
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About}/>
-          <Route path='/sub' component={Sub}/>  
-      </div>
-      </Router>
-    </div>
-  );
-}
+    }
+  >
+    <section>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+    </section>
+  </StickyHeader>
+);
 
 export default App;
